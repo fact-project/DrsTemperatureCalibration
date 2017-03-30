ed5062967ce5755b41ebf6e0d28c53846f674169
Dominik Neise <neised@phys.ethz.ch>
2017-03-30 18:06:28 +0200

    add kwargs

--- drsTemperatureCalibration/my_h5py.py
+++ drsTemperatureCalibration/my_h5py.py
@@ -1,9 +1,10 @@
 import h5py
 
 
-def create_my_dataset(file, name, shape=None, maxshape=None, dtype=None):
+def create_my_dataset(file, name, shape=None, maxshape=None, dtype=None, **kwargs):
     if maxshape is None:
         maxshape = tuple(x if x != 0 else None for x in shape)
+
 
     return file.create_dataset(
         name,
@@ -13,6 +14,7 @@ def create_my_dataset(file, name, shape=None, maxshape=None, dtype=None):
         compression="gzip",
         compression_opts=9,
         fletcher32=True,
+        **kwargs
     )
 
 h5py.File.create_my_dataset = create_my_dataset
