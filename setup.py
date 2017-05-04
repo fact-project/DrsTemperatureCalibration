e17b36e0e2ff7e71334ccdad3b7f8e7de2101fec
Florian Schulz <florian2.schulz@tu-dortmund.de>
2017-05-04 14:10:16 +0200

    update install_requires

--- setup.py
+++ setup.py
@@ -16,20 +16,30 @@ setup(
     },
     tests_require=['pytest>=3.0.0'],
     setup_requires=['pytest-runner'],
-    install_requires=[  # add logging, fact, yaml, h5py, os
+    install_requires=[
         'pandas',
         'numpy',
-        'scipy',
-        'tqdm',
         'click',
+        'pyyaml',
+        'h5py',
+        'tqdm',
         'astropy',
+        'pyfact',
+        'scipy',
         'matplotlib>=1.4',
     ],
     entry_points={'console_scripts': [
-        'drsTempCalib_search_drs_files = drsTemperatureCalibration.drsFitTool:search_drs_files',
-        'drsTempCalib_save_drs_attributes = drsTemperatureCalibration.drsFitTool:save_drs_attributes',
-        'drsTempCalib_save_fit_values = drsTemperatureCalibration.drsFitTool:save_fit_values',
+        ('drsTempCalib_search_drs_files = ' +
+            'drsTemperatureCalibration.drsFitTool:search_drs_files'),
+        ('drsTempCalib_save_drs_attributes =' +
+            'drsTemperatureCalibration.drsFitTool:save_drs_attributes'),
+        ('drsTempCalib_store_interval_indices =' +
+            'drsTemperatureCalibration.drsFitTool:store_source_based_interval_indices'),
+        ('drsTempCalib_save_fit_values =' +
+            'drsTemperatureCalibration.drsFitTool:save_fit_values'),
         'drsTempCalib_make_plots = drsTemperatureCalibration.do:main',
+        ('drsTempCalib_make_plots.pixel_capacitor_drs_values =' +
+            'drsTemperatureCalibration.plotDrsAttributes:pixel_capacitor_drs_values')
     ]},
     zip_safe=False,
 )
