4da65389e4b86536e0d043db8067b351f1b56102
Florian <ImBusiness@gmx.de>
2017-09-04 18:47:11 +0200

    fixed error from preceding rename repository commit

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
     name='drs4Calibration',
     version='0.0.1',
     description='model development for DRS4 amplitude calibration',
-    url='http://github.com/fact-project/drsTemperatureCalibration',
+    url='http://github.com/fact-project/drs4Calibration',
     author='Florian Schulz, Dominik Neise',
     author_email='florian2.schulz@tu-dortmund.de',
     license='MIT',
     packages=[
-        'drsTemperatureCalibration',
+        'drs4Calibration',
     ],
     package_data={
         '': []
@@ -30,16 +30,16 @@ setup(
     ],
     entry_points={'console_scripts': [
         ('drsTempCalib_search_drs_files = ' +
-            'drsTemperatureCalibration.drs4CalibrationTool:search_drs_files'),
+            'drs4Calibration.drs4CalibrationTool:search_drs_files'),
         ('drsTempCalib_save_drs_attributes =' +
-            'drsTemperatureCalibration.drs4CalibrationTool:save_drs_attributes'),
+            'drs4Calibration.drs4CalibrationTool:save_drs_attributes'),
         ('drsTempCalib_store_interval_indices =' +
-            'drsTemperatureCalibration.drs4CalibrationTool:store_source_based_interval_indices'),
+            'drs4Calibration.drs4CalibrationTool:store_source_based_interval_indices'),
         ('drsTempCalib_save_fit_values =' +
-            'drsTemperatureCalibration.drs4CalibrationTool:save_fit_values'),
-        'drsTempCalib_make_plots = drsTemperatureCalibration.do:main',
+            'drs4Calibration.drs4CalibrationTool:save_fit_values'),
+        'drsTempCalib_make_plots = drs4Calibration.do:main',
         ('drsTempCalib_make_plots.pixel_capacitor_drs_values =' +
-            'drsTemperatureCalibration.plotDrsAttributes:pixel_capacitor_drs_values')
+            'drs4Calibration.plotDrsAttributes:pixel_capacitor_drs_values')
     ]},
     zip_safe=False,
 )
